8084e7b08b459b3c4a28450de964af2f20550321
mmmaxwwwell <mmmaxwwwell@github.com>
2021-02-02 22:04:21 -0500

    use subdomain and domain

--- src/email/templates.js
+++ src/email/templates.js
@@ -3,7 +3,7 @@
 const util = require('../service/util');
 
 function verifyKey(ctx, {name, email, nonce, origin, keyId}) {
-  const link = `<a href="https://${process.env.HOSTED_URL}/api/v1/key?op=verify&keyId=${keyId}&nonce=${nonce}" target="_blank">https://${process.env.HOSTED_URL}/api/v1/key?op=verify&keyId=${keyId}&nonce=${nonce}</a>`;
+  const link = `<a href="https://${process.env.KEYSERVER_SUBDOMAIN}${process.env.HOSTED_URL}/api/v1/key?op=verify&keyId=${keyId}&nonce=${nonce}" target="_blank">https://${process.env.KEYSERVER_SUBDOMAIN}${process.env.HOSTED_URL}/api/v1/key?op=verify&keyId=${keyId}&nonce=${nonce}</a>`;
   return {
     subject: ctx.__('verify_key_subject'),
     text: ctx.__('verify_key_text', [name, email, link, process.env.HOSTED_URL])
@@ -11,7 +11,7 @@ function verifyKey(ctx, {name, email, nonce, origin, keyId}) {
 }
 
 function verifyRemove(ctx, {name, email, nonce, origin, keyId}) {
-  const link = `<a href="https://${process.env.HOSTED_URL}/api/v1/key?op=verifyRemove&keyId=${keyId}&nonce=${nonce}" target="_blank">https://${process.env.HOSTED_URL}/api/v1/key?op=verifyRemove&keyId=${keyId}&nonce=${nonce}</a>`;
+  const link = `<a href="https://${process.env.KEYSERVER_SUBDOMAIN}${process.env.HOSTED_URL}/api/v1/key?op=verifyRemove&keyId=${keyId}&nonce=${nonce}" target="_blank">https://${process.env.KEYSERVER_SUBDOMAIN}${process.env.HOSTED_URL}/api/v1/key?op=verifyRemove&keyId=${keyId}&nonce=${nonce}</a>`;
   return {
     subject: ctx.__('verify_removal_subject'),
     text: ctx.__('verify_removal_text', [name, email, process.env.HOSTED_URL, link])
